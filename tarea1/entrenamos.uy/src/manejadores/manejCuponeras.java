7aad515fe7a9f32baa23647d5c256e0b5cd01b5f
maria.galetta@fing.edu.uy <vagrant@kubuntu>
2021-08-18 21:24:24 -0300

    manejador cup

--- tarea1/entrenamos.uy/src/manejadores/manejCuponeras.java
+++ tarea1/entrenamos.uy/src/manejadores/manejCuponeras.java
@@ -1,5 +1,24 @@
 package manejadores;
 
-public class manejCuponeras {
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import logica.InstitucionDeportiva;
+import logica.ActividadDeportiva;
+import logica.Cuponera;
 
+public class manejCuponeras {
+	private Map<String, Cuponera> Cuponeras;
+	
+    private static manejCuponeras instancia = null;
+
+    private manejCuponeras() {
+        institucionesDeportivas = new HashMap<String, InstitucionDeportiva>();
+    }
+    
+    public static manejDeportivas getinstance() {
+        if (instancia == null)
+            instancia = new manejDeportivas();
+        return instancia;
+    }
 }
