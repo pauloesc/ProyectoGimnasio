a6557a3d25894c9a67428a547a51b5780ffc7861
Paulo Elder Sanchez Cuello <paulo.sanchez@fing.edu.uy>
2021-08-20 16:06:48 -0300

    construido

--- tarea1/entrenamos.uy/src/datatypes/InfoBasicaUser.java
+++ tarea1/entrenamos.uy/src/datatypes/InfoBasicaUser.java
@@ -2,4 +2,59 @@ package datatypes;
 
 public class InfoBasicaUser {
 
+	private String nickname;
+	private String nombre;
+	private String apellido;
+	private String correo;
+	private String fechaNac;
+
+	public InfoBasicaUser(String nickname, String nombre, String apellido, String correo, String fechaNac) {
+		this.nickname = nickname;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.correo = correo;
+		this.fechaNac = fechaNac;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getFechaNac() {
+		return fechaNac;
+	}
+
+	public void setFechaNac(String fechaNac) {
+		this.fechaNac = fechaNac;
+	}
+    
+    
 }
