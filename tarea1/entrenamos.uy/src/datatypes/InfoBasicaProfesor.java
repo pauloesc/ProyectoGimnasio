4b36a27c636be6ebe796feefb3b00e35d4e76103
Paulo Elder Sanchez Cuello <paulo.sanchez@fing.edu.uy>
2021-08-20 16:14:46 -0300

    Creado en su totalidad info basica profesor

---
+++ tarea1/entrenamos.uy/src/datatypes/InfoBasicaProfesor.java
@@ -0,0 +1,52 @@
+package datatypes;
+
+public class InfoBasicaProfesor extends InfoBasicaUser {
+
+	private String desc;
+	private String bibliografia;
+	private String url;
+	
+	
+	public InfoBasicaProfesor(String nickname, String nombre, String apellido, String correo, String fechaNac, String des, String bibliografia, String url ) {
+		super(nickname, nombre, apellido, correo, fechaNac);
+		
+		this.desc = des;
+		this.bibliografia = bibliografia;
+		this.url = url;
+		
+	}
+
+
+	public String getDesc() {
+		return desc;
+	}
+
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+
+	public String getBibliografia() {
+		return bibliografia;
+	}
+
+
+	public void setBibliografia(String bibliografia) {
+		this.bibliografia = bibliografia;
+	}
+
+
+	public String getUrl() {
+		return url;
+	}
+
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+	
+}
