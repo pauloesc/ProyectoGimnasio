4c33ae40b610a33f167336337817ae5dabbb7235
Paulo Elder Sanchez Cuello <paulo.sanchez@fing.edu.uy>
2021-08-20 16:09:58 -0300

    creado Info basica socio

---
+++ tarea1/entrenamos.uy/src/datatypes/InfoBasicaSocio.java
@@ -0,0 +1,9 @@
+package datatypes;
+
+public class InfoBasicaSocio extends InfoBasicaUser {
+
+	public InfoBasicaSocio(String nickname, String nombre, String apellido, String correo, String fechaNac) {
+		super(nickname, nombre, apellido, correo, fechaNac);
+	}
+	
+}
