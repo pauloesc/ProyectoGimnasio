63f26dd86ea10f540521ee21eea8ef87cc6f2149
maria.galetta@fing.edu.uy <vagrant@kubuntu>
2021-08-28 03:01:31 -0300

    actualizacion test

--- tarea1/entrenamos.uy/src/tests/TestctrlCuponeras.java
+++ tarea1/entrenamos.uy/src/tests/TestctrlCuponeras.java
@@ -9,15 +9,19 @@ import java.util.Set;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import excepciones.CuponeraNoExisteException;
 import excepciones.CuponeraRepetidaException;
+import logica.Cuponera;
 import logica.DataCuponera;
 import logica.DtClase;
 import logica.Fabrica;
+import logica.IctrlADeportivas;
 import logica.IctrlCuponeras;
+import logica.IctrlIDeportivas;
 
 
 
@@ -29,10 +33,22 @@ class TestctrlCuponeras {
 	@BeforeAll
 	public static void iniciar() {
 		Fabrica fabrica = Fabrica.getInstance();
+		IctrlIDeportivas IID = fabrica.getIctrlIDeportivas();
+		IctrlADeportivas IAD = fabrica.getIctrlADeportivas();
 		ctrlCuponeras = fabrica.getIctrlCuponeras();
+		IID.cargarDatosIDeportivas();
+		IAD.cargarDatosADeportivas();
 	
 	}
 
+	@Test
+	void testlistarCuponeraFal(){
+		
+		assertThrows(CuponeraNoExisteException.class, () ->{ctrlCuponeras.listarCuponeras();});
+	
+}
+	
+	
 	@Test
 	void testregistrarCuponeraExito() {
 		
@@ -93,6 +109,50 @@ void testregistrarCuponeraRepite() {
 	assertThrows(CuponeraRepetidaException.class, () -> {ctrlCuponeras.registrarCuponera("Flexibilidad","Actividades de flexibilidad",new SimpleDateFormat("dd/MM/yy").parse("02/08/21"),new SimpleDateFormat("dd/MM/yy").parse("31/08/21"),20f,new SimpleDateFormat("dd/MM/yy").parse("01/07/21"));});
 
 }
+	
+	@Test
+	void testlistarCuponerasExito() {
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			ctrlCuponeras.registrarCuponera("Baile","Actividades de danza",fi,ff,10f,fa);
+			ctrlCuponeras.registrarCuponera("Didactica","Actividades creativas",fi,ff,10f,fa);
+		} catch (CuponeraRepetidaException e) {
+			e.printStackTrace();
+		}
+		Set<String> rest=new HashSet<String>();
+		try {
+			rest= ctrlCuponeras.listarCuponeras();
+		}catch (CuponeraNoExisteException e) {
+			e.printStackTrace();
+		}	
+	
+		Set<String> com=new HashSet<String>();
+		com.add("Baile");
+		com.add("Didactica");
+		assertEquals(rest,com, "Los sets no son iguales");
+	
 }
 
+
+	 
+	
+	@AfterAll
+	public static void fin() {
+		ctrlCuponeras.cargarDatosCuponeras();
+	}
+	
+
+	
+}
 
