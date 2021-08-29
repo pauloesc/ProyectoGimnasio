198eb35600dc11794d34c04ddfbfc4e836600a2b
maria.galetta@fing.edu.uy <vagrant@kubuntu>
2021-08-29 01:13:11 -0300

    Se agregan test cuponeras fin

--- tarea1/entrenamos.uy/src/tests/TestctrlCuponeras.java
+++ tarea1/entrenamos.uy/src/tests/TestctrlCuponeras.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import excepciones.ActividadDeportivaNoExisteException;
 import excepciones.ActividadDeportivaRepetidaException;
 import excepciones.CuponeraNoExisteException;
 import excepciones.CuponeraRepetidaException;
@@ -124,6 +125,8 @@ void testregistrarCuponeraRepite() {
 	
 	@Test
 	void testlistarCuponerasExito() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
 		Date fi=null;
 		Date ff=null;
 		Date fa=null;
@@ -260,14 +263,334 @@ void testregistrarCuponeraRepite() {
 		
 	}
 		
+	/*public DataCuponera mostrarCuponera (String nomCup) throws CuponeraNoExisteException*/
+	@Test
+	void testDataCuponeraFail () {
+		assertThrows(CuponeraNoExisteException.class, () -> {ctrlCuponeras.mostrarCuponera("Bai");});	
 		
+	}
 	
 	
+	/*public Set<String> listarActividadesfaltantes(String nomcup, String nominst) throws ActividadDeportivaNoExisteException*/
 	
+	@Test
+	void testlistarActividadesfaltantesExito1() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	
+		try {
+			ctrlCuponeras.registrarCuponera("Baile4","Actividades2",fi,ff,10f,fa);
+		}catch (CuponeraRepetidaException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ctrlInsti.altaInstitucion("Ald2", "deporte","url");
+		} catch (InstitucionDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ctrlDeportivas.altaActividadDeportiva("Ald2", "Volei2", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Ald2", "Volei3", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Ald2", "Volei4", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Ald2", "Volei5", "dep", 30f, 300f, fi);
+		} catch (ActividadDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		try {
+			ctrlCuponeras.agregarActividad("Baile4","Volei2", 20);	
+			ctrlCuponeras.agregarActividad("Baile4","Volei5", 20);
+		}catch (ActividadDeportivaRepetidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Set<String> resu=new HashSet<String>();
+		try {
+			resu=ctrlCuponeras.listarActividadesfaltantes("Baile4", "Ald2");
+		} catch (ActividadDeportivaNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Set<String> esperado=new HashSet<String>();
+		esperado.add("Volei3");
+		esperado.add("Volei4");
+		
+		assertEquals(resu,esperado, "Los sets no son iguales");
 	
+		
+	}
 	 
+	@Test
+	void testlistarActividadesfaltantesExito2() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	
+		try {
+			ctrlCuponeras.registrarCuponera("Baile5","Actividades2",fi,ff,10f,fa);
+		}catch (CuponeraRepetidaException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ctrlInsti.altaInstitucion("Ald3", "deporte","url");
+		} catch (InstitucionDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ctrlDeportivas.altaActividadDeportiva("Ald3", "Volei6", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Ald3", "Volei7", "dep", 30f, 300f, fi);
+		} catch (ActividadDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		
+		Set<String> resu=new HashSet<String>();
+		try {
+			resu=ctrlCuponeras.listarActividadesfaltantes("Baile5", "Ald3");
+		} catch (ActividadDeportivaNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Set<String> esperado=new HashSet<String>();
+		esperado.add("Volei6");
+		esperado.add("Volei7");
+		
+		assertEquals(resu,esperado, "Los sets no son iguales");
+	
+		
+	}
+	
+
+	@Test
+	void testlistarActividadesfaltantesFail() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		try {
+			ctrlCuponeras.registrarCuponera("Baile6","Actividades2",fi,ff,10f,fa);
+		}catch (CuponeraRepetidaException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			ctrlInsti.altaInstitucion("Ald4", "deporte","url");
+		} catch (InstitucionDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ctrlDeportivas.altaActividadDeportiva("Ald4", "Volei8", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Ald4", "Volei9", "dep", 30f, 300f, fi);
+		} catch (ActividadDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			ctrlCuponeras.agregarActividad("Baile6","Volei8", 20);	
+			ctrlCuponeras.agregarActividad("Baile6","Volei9", 20);
+		}catch (ActividadDeportivaRepetidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+					
+		assertThrows(ActividadDeportivaNoExisteException.class, () -> {ctrlCuponeras.listarActividadesfaltantes("Baile6", "Ald4");});
+	
+		
+	}
+	
+	/*public Set<String> getCuponerasActividad(String nac) throws CuponeraNoExisteException */
+	
+
+	@Test
+	void testgetCuponeraActividadExito() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		try {
+			ctrlCuponeras.registrarCuponera("ACC1","Actividades2",fi,ff,10f,fa);
+			ctrlCuponeras.registrarCuponera("ACC2","Actividades2",fi,ff,10f,fa);
+			ctrlCuponeras.registrarCuponera("ACC3","Actividades2",fi,ff,10f,fa);
+		}catch (CuponeraRepetidaException e) {
+			e.printStackTrace();
+		}
+	
+		try {
+			ctrlInsti.altaInstitucion("Alp", "deporte","url");
+		} catch (InstitucionDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ctrlDeportivas.altaActividadDeportiva("Alp", "Volt1", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Alp", "Volt2", "dep", 30f, 300f, fi);
+		} catch (ActividadDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			ctrlCuponeras.agregarActividad("ACC1","Volt1", 20);	
+			ctrlCuponeras.agregarActividad("ACC1","Volt2", 20);
+			ctrlCuponeras.agregarActividad("ACC2","Volt1", 20);	
+			ctrlCuponeras.agregarActividad("ACC3","Volt2", 20);
+		
+		}catch (ActividadDeportivaRepetidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Set<String> resu=new HashSet<String>();
+		try {
+			resu=ctrlCuponeras.getCuponerasActividad("Volt2");
+		} catch (CuponeraNoExisteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Set<String> esperado=new HashSet<String>();
+		esperado.add("ACC1");
+		esperado.add("ACC3");
+		
+		assertEquals(resu,esperado, "Los sets no son iguales");
+	
+	}
+	
+	@Test
+	void testgetCuponeraActividadfail1() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		try {
+			ctrlInsti.altaInstitucion("Alp1", "deporte","url");
+		} catch (InstitucionDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ctrlDeportivas.altaActividadDeportiva("Alp1", "Volt3", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Alp1", "Volt4", "dep", 30f, 300f, fi);
+		} catch (ActividadDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+					
+		assertThrows(CuponeraNoExisteException.class, () -> {ctrlCuponeras.getCuponerasActividad("Volt2");});
+		
+	}
+	
+	@Test
+	void testgetCuponeraActividadfail2() {
+		manejCuponeras.getinstance().EliminarManjeador();
+		
+		Date fi=null;
+		Date ff=null;
+		Date fa=null;
+		try {
+			fi = new SimpleDateFormat("dd/MM/yy").parse("05/08/21");
+			ff = new SimpleDateFormat("dd/MM/yy").parse("31/08/21");
+			fa=  new SimpleDateFormat("dd/MM/yy").parse("01/07/21");
+		} catch (ParseException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		try {
+			ctrlCuponeras.registrarCuponera("ACC4","Actividades2",fi,ff,10f,fa);
+		}catch (CuponeraRepetidaException e) {
+			e.printStackTrace();
+		}
+	
+		try {
+			ctrlInsti.altaInstitucion("Alp2", "deporte","url");
+		} catch (InstitucionDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			ctrlDeportivas.altaActividadDeportiva("Alp2", "Volt5", "dep", 30f, 300f, fi);
+			ctrlDeportivas.altaActividadDeportiva("Alp2", "Volt6", "dep", 30f, 300f, fi);
+		} catch (ActividadDeportivaRepetidaException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		try {
+			ctrlCuponeras.agregarActividad("ACC4","Volt6", 20);	
+		
+		}catch (ActividadDeportivaRepetidaException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		assertThrows(CuponeraNoExisteException.class, () -> {ctrlCuponeras.getCuponerasActividad("Volt5");});
+	}
 	
 	
 }
