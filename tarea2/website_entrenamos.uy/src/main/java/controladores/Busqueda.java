c58819da4765a8be656096edd4e4a0978338dba8
Mauricio Barrera <mauricio.barrera@fing.edu.uy>
2021-10-12 12:22:07 -0300

    diseno busqueda

---
+++ tarea2/website_entrenamos.uy/src/main/java/controladores/Busqueda.java
@@ -0,0 +1,45 @@
+package controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import logica.Fabrica;
+import logica.IctrlADeportivas;
+import logica.IctrlCategorias;
+import logica.IctrlClases;
+import logica.IctrlCuponeras;
+import logica.IctrlIDeportivas;
+import logica.IctrlUsuarios;
+
+public class Busqueda extends HttpServlet {
+  
+  private static final long serialVersionUID = 1L;
+
+  public Busqueda() {
+    super();
+  }
+
+  private void processRequest(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    req.getRequestDispatcher("/WEB-INF/busqueda/busqueda.jsp").forward(req, resp);
+
+  }
+  
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+
+    processRequest(request, response);
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+      throws ServletException, IOException {
+
+    processRequest(request, response);
+    
+  }
+
+}
