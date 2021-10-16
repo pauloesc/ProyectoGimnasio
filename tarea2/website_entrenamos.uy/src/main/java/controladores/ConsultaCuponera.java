cdf0c3a95c47ff0d2ca95ab0845b657cff5fc8df
maria.galetta@fing.edu.uy <vagrant@kubuntu>
2021-10-16 14:49:18 -0300

    cambios2

--- tarea2/website_entrenamos.uy/src/main/java/controladores/ConsultaCuponera.java
+++ tarea2/website_entrenamos.uy/src/main/java/controladores/ConsultaCuponera.java
@@ -1,1 +1,70 @@
 package controladores;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import logica.DataCuponera;
+import logica.Fabrica;
+import logica.IctrlCuponeras;
+
+public class ConsultaCuponera extends HttpServlet
+{
+	private static final long serialVersionUID = 1L;
+	
+	private static IctrlCuponeras ctrlCuponeras = Fabrica.getInstance().getIctrlCuponeras();
+	
+	
+	public ConsultaCuponera() 
+	{
+		super();
+		
+	}
+	
+	private void processRequest(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException 
+	{
+		String cup = req.getParameter("cuponera");
+		//verificar que cuponera existe y sino llevar a errorPage
+		DataCuponera cuponera = null;
+		try {
+			cuponera = ConsultaCuponera.getDataCuponera(cup);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		req.setAttribute("cuponera", cuponera);
+		req.getRequestDispatcher("/WEB-INF/actividades/consultaCuponera.jsp").forward(req, resp);
+	}
+	
+		
+	public static DataCuponera getDataCuponera(String cup){
+		DataCuponera dtCuponera;
+		try {
+			dtCuponera = ctrlCuponeras.mostrarCuponera(cup);
+		} 
+		catch(Exception ex) {
+			dtCuponera = null;
+		}
+		return dtCuponera;
+	}
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		processRequest(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
+		processRequest(request, response);
+	}
+}
