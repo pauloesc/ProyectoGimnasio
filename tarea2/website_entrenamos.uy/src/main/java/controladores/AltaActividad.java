a9f2a81e13bbe4472db836df71cfcc6cf7a57bc6
Mauricio Barrera <mauricio.barrera@fing.edu.uy>
2021-11-14 18:53:04 -0300

    saveFile

--- tarea2/website_entrenamos.uy/src/main/java/controladores/AltaActividad.java
+++ tarea2/website_entrenamos.uy/src/main/java/controladores/AltaActividad.java
@@ -1,6 +1,7 @@
 package controladores;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -19,6 +20,8 @@ import publicadores.ActividadDeportivaRepetidaException_Exception;
 import publicadores.WebServicesADeportivas;
 import publicadores.WebServicesADeportivasService;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
+
 import net.java.dev.jaxb.array.StringArray;
 
 
@@ -52,26 +55,18 @@ public class AltaActividad extends HttpServlet {
 		String fileName = null;
 		String ext = null;
 		if ( req.getParts() != null ) {
-			/*gets absolute path of the web application*/
-	        String applicationPath = req.getServletContext().getRealPath("");
-	        // constructs path of the directory to save uploaded file*/
-	        String uploadFilePath = applicationPath + File.separator + UPLOAD_DIR;
-	        
-	        // creates the save directory if it does not exists
-	        File fileSaveDir = new File(uploadFilePath);
-	        if (!fileSaveDir.exists()) {
-	            fileSaveDir.mkdirs();
-	        }
-	        
+			
 	        fileName = nact.toLowerCase().replaceAll("\\s", "");
 	        String nomf = req.getPart("imagenActividad").getSubmittedFileName();
 	        ext = FilenameUtils.getExtension(nomf);
 	        Part part = req.getPart("imagenActividad");
-	        part.write(uploadFilePath + File.separator + fileName + "." + ext);
+	        InputStream file = part.getInputStream();
+	        byte[] bytearr = IOUtils.toByteArray(file);
 			
+	        portActividades.saveFile(bytearr, fileName + "." + ext);
 		}
 		
-        
+   
 		Date date = new Date();
 	
 		GregorianCalendar calendar = new GregorianCalendar();
@@ -90,7 +85,7 @@ public class AltaActividad extends HttpServlet {
 		
 		try {
 			portActividades.altaActividadDeportiva(ninst, nprof, nact, descrip, Float.parseFloat(dur),
-					Float.parseFloat(cost), xmlDate, catsenv, fileName+ "." + ext);
+					Float.parseFloat(cost), xmlDate, catsenv, fileName + "." + ext);
 			req.setAttribute("msjAlta", "La Actividad Deportiva se ha registrado con éxito.");
 			req.setAttribute("estadoAlta", true);
 		} catch (NumberFormatException e) {
