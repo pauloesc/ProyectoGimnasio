c4ce1dc60e100014ea8e513d7abd54fdc9221111
Paulo Elder Sanchez Cuello <paulo.sanchez@fing.edu.uy>
2021-11-23 18:22:15 -0300

    faltaba barra en movil

--- tarea3/websitemovil_entrenamos.uy/src/main/java/publicadores/WebServicesADeportivasService.java
+++ tarea3/websitemovil_entrenamos.uy/src/main/java/publicadores/WebServicesADeportivasService.java
@@ -37,7 +37,7 @@ public class WebServicesADeportivasService
         	String valUrl = "";
         	try {
         		String home = System.getProperty("user.home");
-    	    	FileReader reader = new FileReader( home + "/.entrenamosUy/conf.properties");
+        		FileReader reader = new FileReader( "/"+home+"/.entrenamosUy/conf.properties");
   
     	    	p=new Properties();  
     	    	p.load(reader); 
