839068f8903019d41dcbc3ce5578dff3509e956b
pauloesc <90027706+pauloesc@users.noreply.github.com>
2023-12-25 03:59:45 -0300

    Update ScriptsDeGeneracion.sh
    cambios

--- tarea3/ScriptsDeGeneracion.sh
+++ tarea3/ScriptsDeGeneracion.sh
@@ -6,20 +6,20 @@ mkdir Gen/sv
 
 echo "Generando Website..."
 cd website_entrenamos.uy
-mvn clean compile install
+mvn clean compile package
 cd ..
 cp website_entrenamos.uy/target/web.war Gen/web/web.war
 
 echo "Generando Websitemovil..."
 cd websitemovil_entrenamos.uy
-mvn clean compile install
+mvn clean compile package
 cd ..
 cp websitemovil_entrenamos.uy/target/movil.war Gen/web/movil.war
 
 echo "Generando WorkStation..."
 cd WorkStation
-mvn clean compile install
+mvn clean compile package
 cd ..
 cp WorkStation/target/servidor.jar Gen/sv/servidor.jar
-cp -r WorkStation/files/ Gen/sv/.files/
+cp -r WorkStation/target/.files/ Gen/sv/.files/
 echo "Finalizado"
